e0385ba71f239ac663953e4a6a014f26bc27b38e
shakibo <shaon.softbdltd@gmail.com>
2023-03-30 12:56:24 +0600

    pushing inital project

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+
+const todos = [];
+
+app.get('/', (req, res) => {
+    res.render('pages/index', { todos });
+});
+
+app.post('/', (req, res) => {
+    const { todo, deleteIndex } = req.body;
+
+    if (deleteIndex !== undefined) {
+        todos.splice(deleteIndex, 1);
+    } else if (todo !== '') {
+        todos.push(todo);
+    }
+
+    res.redirect('/');
+});
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
